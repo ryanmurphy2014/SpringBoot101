ca03650632ff4005548f690f8e4992f2f99b81ce
Ryan Murphy <ryanmurphy2014@gmail.com>
2020-03-02 20:08:09 -0500

    Initial commit

---
+++ src/test/java/com/rocktype/springboot101/Springboot101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rocktype.springboot101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot101ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
